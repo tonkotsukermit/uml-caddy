be379150d58643caddd9989711c16266a27790b3
Elliot <45800539+chunk-hunkman@users.noreply.github.com>
2022-11-03 18:09:53 -0600

    added base swagger and fixed query params (#36)

--- cmd/uml-caddy/main_k8s.go
+++ cmd/uml-caddy/main_k8s.go
@@ -6,8 +6,6 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/go-chi/chi/v5"
-
 	"github.com/chunk-hunkman/uml-caddy/pkg/gen"
 	"k8s.io/client-go/util/homedir"
 )
@@ -25,6 +23,12 @@ func init(){
 }
 
 // generateK8sPUML is an http handler that generates a .puml from a given request with query params of "name", "header", and "title"
+// @Summary      Generate a k8s virtual puml document
+// @Param        name   query     string  false  "name"
+// @Param        header query     string  false  "header"
+// @Param        title  query     string  false  "title"
+// @Success      200
+// @Router       /puml/k8s [get]
 func generateK8sPUML(w http.ResponseWriter, req *http.Request) {
 
 	u := newK8sPUML(w, req)
@@ -38,6 +42,13 @@ func generateK8sPUML(w http.ResponseWriter, req *http.Request) {
 }
 
 // generateK8sPNG is an http handler that generates a .png from plant uml from a given request with query params of "name", "header", and "title"
+// @Summary      Generate a k8s infrastructure uml png
+// @Produce      image/png
+// @Param        name   query     string  false  "name"
+// @Param        header query     string  false  "header"
+// @Param        title  query     string  false  "title"
+// @Success      200
+// @Router       /png/k8s [get]
 func generateK8sPNG(w http.ResponseWriter, req *http.Request) {
 
 	buf := new(bytes.Buffer)
@@ -63,6 +74,12 @@ func generateK8sPNG(w http.ResponseWriter, req *http.Request) {
 
 
 // generateK8sInfraPUML is an http handler that generates a .puml from a given request with query params of "name", "header", and "title"
+// @Summary      Generate a k8s infrastructure puml document
+// @Param        name   query     string  false  "name"
+// @Param        header query     string  false  "header"
+// @Param        title  query     string  false  "title"
+// @Success      200
+// @Router       /puml/k8sInfra [get]
 func generateK8sInfraPUML(w http.ResponseWriter, req *http.Request) {
 
 	u := newK8sPUML(w, req)
@@ -76,6 +93,13 @@ func generateK8sInfraPUML(w http.ResponseWriter, req *http.Request) {
 }
 
 // generateK8sInfraPNG is an http handler that generates a .png from plant uml from a given request with query params of "name", "header", and "title"
+// @Summary      Generate a k8s infrastructure uml png
+// @Produce      image/png
+// @Param        name   query     string  false  "name"
+// @Param        header query     string  false  "header"
+// @Param        title  query     string  false  "title"
+// @Success      200
+// @Router       /png/k8sInfra [get]
 func generateK8sInfraPNG(w http.ResponseWriter, req *http.Request) {
 
 	buf := new(bytes.Buffer)
@@ -104,9 +128,9 @@ func newK8sPUML(w http.ResponseWriter, req *http.Request) gen.K8sUML {
 
 	return gen.K8sUML{
 		UML: gen.UML{
-			Name:         checkParam(chi.URLParam(req, "name")),
-			Header:       checkParam(chi.URLParam(req, "header")),
-			Title:        checkParam(chi.URLParam(req, "title")),
+			Name:         checkParam(req.URL.Query().Get("name")),
+			Header:       checkParam(req.URL.Query().Get("header")),
+			Title:        checkParam(req.URL.Query().Get("title")),
 			Output:       w,
 		},
 	}
--- go.mod
+++ go.mod
@@ -2,18 +2,21 @@ module github.com/chunk-hunkman/uml-caddy
 
 go 1.19
 
-require k8s.io/api v0.25.3
+require (
+	github.com/swaggo/swag v1.8.1
+	k8s.io/api v0.25.3
+)
 
 require (
+	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/Masterminds/goutils v1.1.1 // indirect
 	github.com/Masterminds/semver/v3 v3.1.1 // indirect
-	github.com/PuerkitoBio/purell v1.1.1 // indirect
-	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/emicklei/go-restful/v3 v3.8.0 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
-	github.com/go-openapi/jsonreference v0.19.5 // indirect
-	github.com/go-openapi/swag v0.19.14 // indirect
+	github.com/go-openapi/jsonreference v0.20.0 // indirect
+	github.com/go-openapi/spec v0.20.6 // indirect
+	github.com/go-openapi/swag v0.19.15 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/gnostic v0.5.7-v3refs // indirect
 	github.com/google/uuid v1.1.2 // indirect
@@ -27,11 +30,13 @@ require (
 	github.com/shopspring/decimal v1.2.0 // indirect
 	github.com/spf13/cast v1.3.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/swaggo/files v0.0.0-20220610200504-28940afbdbfe // indirect
 	golang.org/x/crypto v0.0.0-20220315160706-3147a52a75dd // indirect
 	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
 	golang.org/x/sys v0.1.0 // indirect
-	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
+	golang.org/x/term v0.1.0 // indirect
 	golang.org/x/time v0.0.0-20220210224613-90d013bbcef8 // indirect
+	golang.org/x/tools v0.2.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
@@ -48,9 +53,10 @@ require (
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/swaggo/http-swagger v1.3.3
 	golang.org/x/exp v0.0.0-20221031165847-c99f073a8326
-	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
-	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/net v0.1.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	k8s.io/apimachinery v0.25.3
